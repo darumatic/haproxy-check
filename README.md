e1d3638683de077f6332895ec40edc15ede96a65
BrandonDarumatic <brandon@darumatic.com>
2018-10-23 17:44:55 +1100

    Support define target backends

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ haproxy-check
 This script is to check the connection status of the haproxy backend servers.
 It updates the haproxy stats files to disable/enable the backend servers depend on the time-to-first-byte.
 
+
+Usage:
+
+```bash
+$ haproxy_check.sh
+```
+
 This script should run periodically with the cron job.
+such as: `*/5 * * * * /root/haproxy_check.sh`
--- haproxy_check.sh
+++ haproxy_check.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
-
+#CONF                           the haproxy configuration file path i.g. /etc/haproxy/haproxy.cfg
 #DOMAIN_TEST                    domain name to test backends with
-#TTFB_TEST=5                    max time-to-first-byte value
-#BACKEND_PATTERN="p-icp-\d+"    pattern to detect backend hostnames in  haproxy config file
-#BACKEND_PORT=8080              http port to test backends with
-
+#TTFB_TEST                      max time-to-first-byte value
+#LOG_FILE                       the log file path
+#TEST_BACKENDS                  The array of target backend, e.g. TEST_BACKENDS=("web" "websocket")
+#TEST_PORTS                     The array of test port of target backend, e.g. TEST_PORTS=("80" "8080")
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
@@ -13,33 +13,39 @@ if ! which nc &>/dev/null
 then echo "install nc , e.g. yum install nc" ; exit
 fi
 
+if ! which ts &>/dev/null
+then echo "install moreutils , e.g. yum install moreutils" ; exit
+fi
+
+echo_to_file(){
+    echo $1 | ts | tee -a $LOG_FILE
+}
+
 disable_backend_server() {
-    local BACKEND=$1
-    echo "==disabling backend $BACKEND"
-    echo "disable server icp/$BACKEND"      | nc -U /var/lib/haproxy/stats
-    echo "disable server icp-ssl/$BACKEND"  | nc -U /var/lib/haproxy/stats
+local SERVER=$1
+echo_to_file "==disabling backend $BACKEND_NAME/$SERVER"
+echo "disable server $BACKEND_NAME/$SERVER"      | nc -U /var/lib/haproxy/stats
 }
 
 enable_backend_server() {
-    local BACKEND=$1
-    echo "==enabling backend $BACKEND"
-    echo "enable server icp/$BACKEND"      | nc -U /var/lib/haproxy/stats
-    echo "enable server icp-ssl/$BACKEND"  | nc -U /var/lib/haproxy/stats
+local SERVER=$1
+echo_to_file "==enabling backend $BACKEND_NAME/$SERVER"
+echo "enable server $BACKEND_NAME/$SERVER"      | nc -U /var/lib/haproxy/stats
 }
 
 list_enabled_backend_servers() {
 echo "show stat" | nc -U /var/lib/haproxy/stats | \
-    grep  -P ",$BACKEND_PATTERN" | awk -F, '{if ($18 == "UP") print $2 }' | sort -u
+    awk -F, '{if ($1 == "'$BACKEND_NAME'" && $2 != "BACKEND" && $18 == "UP") print $2 }' | sort -u
 }
 
 list_disabled_backend_servers() {
 echo "show stat" | nc -U /var/lib/haproxy/stats | \
-    grep  -P ",$BACKEND_PATTERN" | awk -F, '{if ($18 == "MAINT") print $2 }' | sort -u
+    awk -F, '{if ($1 == "'$BACKEND_NAME'" && $2 != "BACKEND" && $18 == "MAINT") print $2 }' | sort -u
 }
 
-get_backend_ip() {
-local BACKEND=$1
-cat $CONF| grep -oP 'server\s+'$BACKEND'\s+[\d\.]+(?=:)'  | awk '{print $3}' | sort -u
+get_server_ip() {
+local SERVER=$1
+cat $CONF| grep -oP 'server\s+'$SERVER'\s+[a-zA-Z0-9\-\.]*'  | awk '{print $3}' | sort -u | head -n 1
 }
 
 get_ttfb() {
@@ -47,42 +53,56 @@ local TTFB=$( curl  http://$SERVER_IP:$BACKEND_PORT --header "Host: $DOMAIN"  -w
 echo $TTFB
 }
 
-echo
-echo "===testing disabled backends"
-for BACKEND in `list_disabled_backend_servers`
-do
-    SERVER_IP=$( get_backend_ip $BACKEND)
-    DOMAIN=$DOMAIN_TEST
+test_backend(){
+
+    local BACKEND_NAME=$1
+    local BACKEND_PORT=$2
+
     echo
-    echo "= testing $BACKEND / $SERVER_IP "
-    TTFB=$( get_ttfb )
-    echo "= got TTFB $TTFB"
-    if perl -e ' exit  ( '$TTFB' < '$TTFB_TEST' ? 0 : 1 )  '
-    then    enable_backend_server $BACKEND
-    else    echo "= do nothing"
+    echo "===testing disabled servers of backend $BACKEND_NAME"
+    for SERVER in `list_disabled_backend_servers`
+    do
+        SERVER_IP=$( get_server_ip $SERVER)
+        DOMAIN=$DOMAIN_TEST
+        echo
+        echo "= testing $SERVER / $SERVER_IP:$BACKEND_PORT "
+        TTFB=$( get_ttfb )
+        echo "= got TTFB $TTFB"
+        if perl -e ' exit  ( '$TTFB' < '$TTFB_TEST' ? 0 : 1 )  '
+        then    enable_backend_server $SERVER
+        else    echo "= do nothing"
+        fi
+    done
+
+    echo
+
+    echo "===testing enabled servers of backend $BACKEND_NAME"
+    for SERVER in `list_enabled_backend_servers`
+    do
+        SERVER_IP=$( get_server_ip $SERVER)
+        DOMAIN=$DOMAIN_TEST
+        echo
+        echo "= testing $SERVER / $SERVER_IP:$BACKEND_PORT "
+        TTFB=$( get_ttfb )
+        echo "= got TTFB $TTFB"
+        if perl -e ' exit  ( '$TTFB' >  '$TTFB_TEST' ? 0 : 1 )  '
+        then    disable_backend_server $SERVER
+        else    echo "= do nothing"
+        fi
+    done
+
+    if [ $( list_enabled_backend_servers | wc -l ) == 0 ]
+    then
+    #enable random disabled server
+    enable_backend_server $( list_disabled_backend_servers | sort -R | head -n1 )
     fi
+}
+
+for i in ${!TEST_BACKENDS[@]};
+do
+  TEST_BACKEND=${TEST_BACKENDS[$i]}
+  TEST_PORT=${TEST_PORTS[$i]}
+  test_backend $TEST_BACKEND $TEST_PORT
 done
 
-echo
-echo "===testing enabled  backends"
-for BACKEND in `list_enabled_backend_servers`
-do
-    SERVER_IP=$( get_backend_ip $BACKEND)
-    DOMAIN=$DOMAIN_TEST
-    echo
-    echo "= testing $BACKEND / $SERVER_IP "
-    TTFB=$( get_ttfb )
-    echo "= got TTFB $TTFB"
-    if perl -e ' exit  ( '$TTFB' >  '$TTFB_TEST' ? 0 : 1 )  '
-    then    disable_backend_server $BACKEND
-    else    echo "= do nothing"
-    fi
-done
 
-
-
-if [ $( list_enabled_backend_servers | wc -l ) == 0 ]
-then
-   #enable random disabled server
-   enable_backend_server $( list_disabled_backend_servers | sort -R | head -n1 )
-fi
